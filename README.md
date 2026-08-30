712ff0a012efd7f7ae8e4718e808693d79431048
Loick01 <loicker89@gmail.com>
2026-08-31 00:50:50 +0200

    Add glm, glad, glfw and imgui

---
+++ README.md
@@ -0,0 +1,1 @@
+# OpenVoxL (WIP)
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+int main()
+{
+    std::cout << "Hello OpenVoxL\n";
+}
